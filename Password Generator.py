0313a3b39c82f8c07d8d3e82e339ee6d46bcefb3
jahaa032 <jahaa032@osloskolen.no>
2025-09-05 14:32:24 +0200

    Mer hint

--- Password Generator.py
+++ Password Generator.py
@@ -2,16 +2,27 @@
 import random
 import string
 
-passord = ''.join(random.choice(string.ascii_letters) for _ in range(4))
 
-print("Generate Passord", passord)
+def restart():
 
+    passord = ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(6)).lower()
+    passord_list = [letter for letter in passord]
+    print("Debug passord", passord)
+
+    while True:
+            guess = input("Gjett passordet med 6 ting eller skriv quit for å gi opp: ").lower()
+            if guess == 'quit':
+                 print("Imagia å gi opp, taper ass.")
+                 break
+            elif guess == passord:
+                print("Du gjettet passordet gratulerer 🥂🥂")
+                break
+            elif:
+                riktig_pososjon = sum(a == b for a, b in zip(guess, passord))
+                print(f"❌Feil prøv igjen {riktig_pososjon} ting i riktig possisjon")
 while True:
-        guess1 = input("Gjett passordet med 4 bokstaver: ")
-
-        if guess1 == passord:
-            print("Du gjettet passordet gratulerer 🥂🥂")
-            break
-        else:
-            riktig_pososjon = sum(a == b for a, b in zip(guess1, passord))
-            print(f"❌Feil prøv igjen {riktig_pososjon} bokstaver i riktig possisjon")
+     restart()
+     again = input("Vil du starte et nytt spill?: (yes/no)")
+     if again != "yes":
+        print("Taper fr, hadde")
+        break
